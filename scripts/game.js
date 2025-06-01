7419aae753ca976828192a468c678817353f7b4b
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-06-01 14:40:34 +0200

    added movableObject class

--- scripts/game.js
+++ scripts/game.js
@@ -1,14 +1,10 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new MovableObject();
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    character.src = '../assets/img/2_character_pepe/1_idle/idle/I-1.png';
-
-    character.onload = () => {
-        ctx.drawImage(character, 20, 300, 50, 150);
-    };
     
+    console.log('my character: ', character);
 }
