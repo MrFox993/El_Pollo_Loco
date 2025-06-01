278e0313ee23b76a82b160ceb3d0c34dd508b83d
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-06-01 15:30:30 +0200

    show charcter on loaded world

--- classes/character.class.js
+++ classes/character.class.js
@@ -5,8 +5,8 @@ class Character extends MovableObject {
         this.loadImage('../assets/img/2_character_pepe/1_idle/idle/I-1.png');
         this.x = 100;
         this.y = 200;
-        this.width = 50;
-        this.height = 100;
+        this.width = 150;
+        this.height = 250;
     }
 
     jump() {
