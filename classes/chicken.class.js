9afca522912e47bd36228f900451df90a43cbec5
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-06-01 15:57:00 +0200

    randomized position of enemies on load

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -3,8 +3,8 @@ class Chicken extends MovableObject{
     constructor() {
         super();
         this.loadImage('../assets/img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
-        this.x = 500
-        this.y = 370
+        this.x = 300 + Math.random() * 400; // Random x position
+        this.y = 370 
         this.width = 60;
         this.height = 60;
     }
