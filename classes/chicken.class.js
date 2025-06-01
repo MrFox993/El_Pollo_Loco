8ddfbc364ba4f5f521cb79ba18a1f967781b692d
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-06-01 15:17:25 +0200

    added constructor for chicken

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -1,3 +1,12 @@
 class Chicken extends MovableObject{
 
+    constructor() {
+        super();
+        this.loadImage('../assets/img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
+        this.x = 500
+        this.y = 370
+        this.width = 60;
+        this.height = 60;
+    }
+
 }
