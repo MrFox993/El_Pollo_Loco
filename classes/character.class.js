f447d623f5aab8ec36270d73bfed44e7364da33c
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-06-01 15:17:17 +0200

    added constructor for character

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,4 +1,14 @@
 class Character extends MovableObject {
+
+    constructor() {
+        super();
+        this.loadImage('../assets/img/2_character_pepe/1_idle/idle/I-1.png');
+        this.x = 100;
+        this.y = 200;
+        this.width = 50;
+        this.height = 100;
+    }
+
     jump() {
         console.log('Jumping');
     }
