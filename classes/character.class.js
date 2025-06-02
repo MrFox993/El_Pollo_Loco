76f7676f496def9dd69c69cf7b23966d784395e5
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-06-02 09:41:55 +0200

    scaled character

--- classes/character.class.js
+++ classes/character.class.js
@@ -3,10 +3,10 @@ class Character extends MovableObject {
     constructor() {
         super();
         this.loadImage('../assets/img/2_character_pepe/1_idle/idle/I-1.png');
-        this.x = 100;
-        this.y = 200;
+        this.x = 0;
+        this.y = 130;
         this.width = 150;
-        this.height = 250;
+        this.height = 300;
     }
 
     jump() {
