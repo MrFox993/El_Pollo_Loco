c2e201f70c14a9a792d06c7e819a48720c2fb2fc
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-06-03 15:51:17 +0200

    implemented animation of clouds

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -7,6 +7,13 @@ class Cloud extends MovableObject {
         super();
         this.loadImage('../assets/img/5_background/layers/4_clouds/1.png');
         this.x = Math.random() * 700; // Random x position
+        this.animate();
+    }
 
+    animate() {
+        setInterval(() => {
+            this.x -= 0.15;
+        }, 1000 / 60);
+        
     }
 }
