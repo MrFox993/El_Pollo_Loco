52c8cbb6bac5f04cae7e87c7f85bbecf98724dab
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-06-03 16:31:30 +0200

    feat: add idle animation for character with image caching

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,12 +1,37 @@
 class Character extends MovableObject {
+    imagesIdle = [
+        '../assets/img/2_character_pepe/1_idle/idle/I-1.png',
+        '../assets/img/2_character_pepe/1_idle/idle/I-2.png',
+        '../assets/img/2_character_pepe/1_idle/idle/I-3.png',
+        '../assets/img/2_character_pepe/1_idle/idle/I-4.png',
+        '../assets/img/2_character_pepe/1_idle/idle/I-5.png',
+        '../assets/img/2_character_pepe/1_idle/idle/I-6.png',
+        '../assets/img/2_character_pepe/1_idle/idle/I-7.png',
+        '../assets/img/2_character_pepe/1_idle/idle/I-8.png',
+        '../assets/img/2_character_pepe/1_idle/idle/I-9.png',
+        '../assets/img/2_character_pepe/1_idle/idle/I-10.png'
+    ];
+
+    currentImageIndex = 0;
 
     constructor() {
         super();
-        this.loadImage('../assets/img/2_character_pepe/1_idle/idle/I-1.png');
         this.x = 0;
         this.y = 130;
         this.width = 150;
         this.height = 300;
+        this.loadImage('../assets/img/2_character_pepe/1_idle/idle/I-1.png');
+        this.loadImages(this.imagesIdle);
+        this.animate();
+    }
+
+    animate() {
+            setInterval(() => {
+                let index = this.currentImageIndex % this.imagesIdle.length;
+                let imagePath = this.imagesIdle[index];
+                this.img = this.imageCache[imagePath];
+                this.currentImageIndex++;    
+            }, 1000 / 4); 
     }
 
     jump() {
--- classes/movableObject.class.js
+++ classes/movableObject.class.js
@@ -2,10 +2,19 @@ class MovableObject {
     x;
     y;
     img;
+    imageCache = {};
 
     loadImage(imagePath) {
         this.img = new Image();
         this.img.src = imagePath;
+    }
+
+    loadImages(array) {
+        array.forEach(path => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
     }
 
     moveRight() {
