9e51358b854b4ecaa1e296e8981fc8418f7365ce
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-06-06 20:19:45 +0200

    changed key codes of w a s d

--- scripts/game.js
+++ scripts/game.js
@@ -11,13 +11,13 @@ window.addEventListener('keydown', (event) => {
     if (event.keyCode === 68 || event.keyCode === 39) {
         keyboard.right = true;
     }
-    if (event.keyCode === 97 || event.keyCode === 37) {
+    if (event.keyCode === 65 || event.keyCode === 37) {
         keyboard.left = true;
     }
-    if (event.keyCode === 119 || event.keyCode === 38) {
+    if (event.keyCode === 87 || event.keyCode === 38) {
         keyboard.up = true;
     }
-    if (event.keyCode === 115 || event.keyCode === 40) {
+    if (event.keyCode === 83 || event.keyCode === 40) {
         keyboard.down = true;
     }
     if (event.keyCode === 32) {
@@ -27,16 +27,16 @@ window.addEventListener('keydown', (event) => {
 });
 
 window.addEventListener('keyup', (event) => {
-    if (event.keyCode === 100 || event.keyCode === 39) {
+    if (event.keyCode === 68 || event.keyCode === 39) {
         keyboard.right = false;
     }
-    if (event.keyCode === 97 || event.keyCode === 37) {
+    if (event.keyCode === 65 || event.keyCode === 37) {
         keyboard.left = false;
     }
-    if (event.keyCode === 119 || event.keyCode === 38) {
+    if (event.keyCode === 87 || event.keyCode === 38) {
         keyboard.up = false;
     }
-    if (event.keyCode === 115 || event.keyCode === 40) {
+    if (event.keyCode === 83 || event.keyCode === 40) {
         keyboard.down = false;
     }
     if (event.keyCode === 32) {
