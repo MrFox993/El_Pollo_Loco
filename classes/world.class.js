b39d7e3370ed9cd4fe139b25997b2cd703f2bb01
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2025-06-08 22:01:19 +0200

    feat: refactor World class to use level data

--- classes/world.class.js
+++ classes/world.class.js
@@ -1,28 +1,10 @@
 class World {
     character = new Character();
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Chicken()
-    ];
-    clouds = [
-        new Cloud()
-    ]
-    backgroundObjects = [];
-    backgroundImages_1 = [
-        './assets/img/5_background/layers/air.png',
-        './assets/img/5_background/layers/3_third_layer/1.png',
-        './assets/img/5_background/layers/2_second_layer/1.png',
-        './assets/img/5_background/layers/1_first_layer/1.png'
-    ];
-    backgroundImages_2 = [
-        './assets/img/5_background/layers/air.png',
-        './assets/img/5_background/layers/3_third_layer/2.png',
-        './assets/img/5_background/layers/2_second_layer/2.png',
-        './assets/img/5_background/layers/1_first_layer/2.png'
-    ];
-    bgImgStartingX = -3595;
+    enemies = level_1.enemies;
+    clouds = level_1.clouds;
+    backgroundObjects = level_1.backgroundObjects;
+    backgroundImages_1;
+    backgroundImages_2;
     ctx;
     canvas;
     keyboard;
@@ -32,7 +14,6 @@ class World {
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
         this.keyboard = keyboard;
-        this.generateBackgroundObjects();
         this.draw();
         this.setWorld();
     }
@@ -83,23 +64,5 @@ class World {
         objects.forEach(obj => {
             this.addToMap(obj);
         });
-    }
-    
-    generateBackgroundObjects() {
-        const repetitions = 10;
-        const segmentWidth = 719;
-        let currentX = this.bgImgStartingX;
-
-        this.backgroundObjects = []; 
-
-        for (let i = 0; i < repetitions; i++) {
-            const imageSet = i % 2 === 0 ? this.backgroundImages_1 : this.backgroundImages_2;
-
-            for (let j = 0; j < imageSet.length; j++) {
-                this.backgroundObjects.push(new BackgroundObject(imageSet[j], currentX));
-            }
-
-            currentX += segmentWidth;
-        }
     }
 }
---
+++ levels/level1.js
@@ -0,0 +1,23 @@
+const level_1 = new Level(
+    [
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Chicken()
+    ],
+    [
+        new Cloud()
+    ],
+    [
+        './assets/img/5_background/layers/air.png',
+        './assets/img/5_background/layers/3_third_layer/1.png',
+        './assets/img/5_background/layers/2_second_layer/1.png',
+        './assets/img/5_background/layers/1_first_layer/1.png'
+    ],
+    [
+        './assets/img/5_background/layers/air.png',
+        './assets/img/5_background/layers/3_third_layer/2.png',
+        './assets/img/5_background/layers/2_second_layer/2.png',
+        './assets/img/5_background/layers/1_first_layer/2.png'
+    ]
+);
