00e3614f1b03e994e2bb5e295c9ef84cfa9e8956
Marcel Fuchs <mfuchs@phoenixcontact.com>
2025-12-01 07:51:37 +0100

    refactor: changed init of level for endboss actions

--- levels/level1.js
+++ levels/level1.js
@@ -1,18 +1,21 @@
-const level_1 = new Level(
-  [new ChickenSmall(), new ChickenSmall(), new ChickenSmall(), new ChickenSmall(), new Endboss()],
-  [new Cloud(), new Cloud(), new Cloud(), new Cloud(), new Cloud()],
-  [
-    "./assets/img/5_background/layers/air.png",
-    "./assets/img/5_background/layers/3_third_layer/1.png",
-    "./assets/img/5_background/layers/2_second_layer/1.png",
-    "./assets/img/5_background/layers/1_first_layer/1.png",
-  ],
-  [
-    "./assets/img/5_background/layers/air.png",
-    "./assets/img/5_background/layers/3_third_layer/2.png",
-    "./assets/img/5_background/layers/2_second_layer/2.png",
-    "./assets/img/5_background/layers/1_first_layer/2.png",
-  ],
-  [new Bottle(), new Bottle(), new Bottle(), new Bottle(), new Bottle(), new Bottle(), new Bottle()],
-  [new Coin(), new Coin(), new Coin(), new Coin(), new Coin(), new Coin(), new Coin()]
+
+const level_1 = new Level({
+    enemies: [new ChickenSmall(), new ChickenSmall(), new ChickenSmall(), new ChickenSmall()], 
+    endboss: new Endboss(), 
+    clouds: [new Cloud(), new Cloud(), new Cloud(), new Cloud(), new Cloud()],
+    backgroundObject1: [
+        "./assets/img/5_background/layers/air.png",
+        "./assets/img/5_background/layers/3_third_layer/1.png",
+        "./assets/img/5_background/layers/2_second_layer/1.png",
+        "./assets/img/5_background/layers/1_first_layer/1.png",
+    ],
+    backgroundObject2: [
+        "./assets/img/5_background/layers/air.png",
+        "./assets/img/5_background/layers/3_third_layer/2.png",
+        "./assets/img/5_background/layers/2_second_layer/2.png",
+        "./assets/img/5_background/layers/1_first_layer/2.png",
+    ],
+    bottles: [new Bottle(), new Bottle(), new Bottle(), new Bottle(), new Bottle(), new Bottle(), new Bottle()], 
+    coins: [new Coin(), new Coin(), new Coin(), new Coin(), new Coin(), new Coin(), new Coin()] 
+  }
 );
