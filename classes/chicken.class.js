6ebfbc676fd5d263ef2713ef68d0a408d5a24378
Marcel Fuchs <mfuchs@phoenixcontact.com>
2025-12-05 08:44:15 +0100

    feat: added ChickenBig class

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -32,3 +32,38 @@ class ChickenSmall extends MovableObject {
     }, 1000 / 60);
   }
 }
+
+class ChickenBig extends MovableObject {
+  imagesBigChickenWalking = [
+    "./assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+    "./assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+    "./assets/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+  ];
+  imagesBigChickenDead = [
+    "./assets/img/3_enemies_chicken/chicken_normal/2_dead/dead.png"
+  ]
+  speed = 0.5;
+
+  constructor() {
+    super();
+    this.x = 650 + Math.random() * 600; // Random x position
+    this.y = 370;
+    this.width = 60;
+    this.height = 60;
+    this.speed = 0.25 + Math.random() * 0.5;
+    this.loadImage("./assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+    this.loadImages(this.imagesBigChickenWalking);
+    this.loadImages(this.imagesBigChickenDead)
+  }
+
+  startAnimation() {
+    if (!gameStarted) return;
+    if (gameOver) return;
+    setInterval(() => {
+      this.playAnimation(this.imagesBigChickenWalking);
+    }, 1000 / 2);
+    setInterval(() => {
+      this.moveLeft();
+    }, 1000 / 60);
+  }
+}
