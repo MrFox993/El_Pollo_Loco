ec9e30929e9ce1b5eaa38214e9890957a99adc94
Marcel Fuchs <mfuchs@phoenixcontact.com>
2026-02-27 14:25:49 +0100

    FEAT: make collision math more robost to missing offsets

--- classes/movableObject.class.js
+++ classes/movableObject.class.js
@@ -92,13 +92,16 @@ class MovableObject extends DrawableObject {
    * @param {MovableObject} mObject - The other object to test against.
    * @returns {boolean} True if bounding boxes overlap, else false.
    */
-  isColliding(mObject) {
-    return (
-      this.x + this.width - this.offset.right > mObject.x - mObject.offset.left &&
-      this.x + this.offset.left < mObject.x + mObject.width - mObject.offset.right &&
-      this.y + this.height - this.offset.bottom > mObject.y + mObject.offset.top &&
-      this.y + this.offset.top < mObject.y + mObject.height - mObject.offset.bottom
-    );
+  isColliding(mObject) { 
+    const myOff = this.offset || { top:0, bottom:0, left:0, right:0 }; 
+    const otherOff = (mObject && mObject.offset) ? mObject.offset : { top:0, bottom:0, left:0, right:0 };
+
+    return ( 
+      this.x + this.width - myOff.right > mObject.x - otherOff.left &&
+      this.x + myOff.left < mObject.x + mObject.width - otherOff.right &&
+      this.y + this.height - myOff.bottom > mObject.y + otherOff.top && 
+      this.y + myOff.top < mObject.y + mObject.height - otherOff.bottom 
+    ); 
   }
 
   /**
